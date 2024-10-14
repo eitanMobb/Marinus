a95bca69abb0cf6ba41c929d035c2ffe768e08d3
Mobb autofixer <git@mobb.ai>
2024-10-14 18:02:14 +0000

    mobb fix commit: 31abbcd8-29f2-4b13-9bf3-e32cfd85e6e4

--- web_server/config/models/iblox_a_records.js
+++ web_server/config/models/iblox_a_records.js
@@ -12,6 +12,7 @@
  * governing permissions and limitations under the License.
  */
 
+const mongoSanitize = require('express-mongo-sanitize');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -48,7 +49,7 @@ module.exports = {
     },
     getIBAddrByIPPromise: function (ip) {
         return ipAddrModel.find({
-            'ipv4addr': ip,
+            'ipv4addr': mongoSanitize.sanitize({ data: ip }).data,
         }).exec();
     },
     getIBAddrByIPRangePromise: function (ipRange) {
