bbc9f7f203fe0fbe49a193b8498374660397cebc
Mobb autofixer <git@mobb.ai>
2024-10-14 18:26:05 +0000

    mobb fix commit: baad7e6b-d2b1-4949-ae6d-a96c24d265eb

--- web_server/config/models/virustotal.js
+++ web_server/config/models/virustotal.js
@@ -12,6 +12,7 @@
  * governing permissions and limitations under the License.
  */
 
+const mongoSanitize = require('express-mongo-sanitize');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -312,7 +313,7 @@ module.exports = {
     },
     getWhoisByZonePromise: function (zone) {
         return virustotalModel.find({
-            'zone': zone,
+            'zone': mongoSanitize.sanitize({ data: zone }).data,
         }, {
             'zone': 1,
             'whois': 1,
